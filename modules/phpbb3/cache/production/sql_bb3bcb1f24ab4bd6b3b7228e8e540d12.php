d3f0dcb8b5c23891e09e3d6b454ee857accae87e
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-07 10:38:57 -0400

    Initial Module Commit

---
+++ modules/phpbb3/cache/production/sql_bb3bcb1f24ab4bd6b3b7228e8e540d12.php
@@ -0,0 +1,5 @@
+<?php exit; ?>
+1662547658
+SELECT f.forum_id, f.forum_name, f.parent_id, f.forum_type, f.left_id, f.right_id FROM (an602_forums f) ORDER BY f.left_id ASC
+511
+a:3:{i:0;a:6:{s:8:"forum_id";s:1:"1";s:10:"forum_name";s:19:"Your first category";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"0";s:7:"left_id";s:1:"1";s:8:"right_id";s:1:"6";}i:1;a:6:{s:8:"forum_id";s:1:"2";s:10:"forum_name";s:16:"Your first forum";s:9:"parent_id";s:1:"1";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"2";s:8:"right_id";s:1:"5";}i:2;a:6:{s:8:"forum_id";s:1:"3";s:10:"forum_name";s:9:"sub forum";s:9:"parent_id";s:1:"2";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"3";s:8:"right_id";s:1:"4";}}
